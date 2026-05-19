7c2a6bff7545bd68f80871572c61a88c6f965830
franklinCamposGuillen <franklin.campos.27@unsch.edu.pe>
2026-05-19 17:27:58 -0500

    Primera versión - Sistema de exámenes

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xvewshifcjjzgbyaasls.supabase.co'  // ← PEGA TU URL AQUÍ
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2ZXdzaGlmY2pqemdieWFhc2xzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4OTU5MDEsImV4cCI6MjA5NDQ3MTkwMX0.W2jr1phXZFiRQGPdwpUVu1876cJpY1MOzrpMoq20ing'       // ← PEGA TU ANON KEY AQUÍ
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
